93116ff62e21aedb8fc6ce6654b2e3976434598b
劉敦桐 <zse811022@hotmail.com>
2023-09-11 20:34:01 +0800

    Add project files.

---
+++ Data/WebApplication3_DAY11Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3_DAY11.Data
+{
+    public class WebApplication3_DAY11Context : DbContext
+    {
+        public WebApplication3_DAY11Context() : base("name=WebApplication3_DAY11Context")
+        {
+        }
+
+		public System.Data.Entity.DbSet<WebApplication3_DAY11.Models.Student> Students { get; set; }
+	}
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication3_DAY11.MvcApplication" Language="C#" %>
